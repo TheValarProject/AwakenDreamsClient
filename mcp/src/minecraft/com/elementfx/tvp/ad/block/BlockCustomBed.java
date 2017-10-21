354e0fe4381f9ca2f2bd5941893a64a7e46c9dc8
MartyParty21 <martin.rokyta@seznam.cz>
2017-10-21 18:28:41 +0200

    Add Bunk Beds

--- mcp/src/minecraft/com/elementfx/tvp/ad/block/BlockCustomBed.java
+++ mcp/src/minecraft/com/elementfx/tvp/ad/block/BlockCustomBed.java
@@ -1,22 +1,30 @@
 package com.elementfx.tvp.ad.block;
 
+import java.util.List;
 import java.util.Random;
 import javax.annotation.Nullable;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockBed;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.BlockFenceGate;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.EnumPushReaction;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemLead;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
@@ -33,12 +41,19 @@ import net.minecraft.world.World;
 public class BlockCustomBed extends BlockBed
 {
 	private Item inventoryItem = null;
+	/** Whether this bed is placed below another one */
+    public static final PropertyBool BUNK = PropertyBool.create("bunk");
+
+    protected static final AxisAlignedBB[] BOUNDING_BOXES = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5625D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D)};
+    public static final AxisAlignedBB BUNK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+
 
     public BlockCustomBed()
     {
         super();
         this.setHardness(0.2F);
         this.setSoundType(SoundType.CLOTH);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(BUNK, Boolean.valueOf(false)));
     }
     
     // The item cannot be set in the constructor because the members of Items aren't initialized yet
@@ -63,5 +78,78 @@ public class BlockCustomBed extends BlockBed
     public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
     {
         return new ItemStack(this.inventoryItem);
+    }
+
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn)
+    {
+        state = state.getActualState(worldIn, pos);
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, BED_AABB);
+
+        if (((Boolean)state.getValue(BUNK)).booleanValue())
+        {
+            addCollisionBoxToList(pos, entityBox, collidingBoxes, BUNK_AABB);
+        }
+
+    }
+    
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        state = this.getActualState(state, source, pos);
+        return BOUNDING_BOXES[getBoundingBoxIdx(state)];
+    }
+
+    /**
+     * Returns the correct index into boundingBoxes, based on what the fence is connected to.
+     */
+    private static int getBoundingBoxIdx(IBlockState state)
+    {
+        int i = 0;
+
+        if (((Boolean)state.getValue(BUNK)).booleanValue())
+        {
+            i |= 1;
+        }
+
+        return i;
+    }
+
+    public boolean canConnectTo(IBlockAccess worldIn, BlockPos pos)
+    {
+    	IBlockState iblockstate = worldIn.getBlockState(pos);
+        Block block = iblockstate.getBlock();
+        IBlockState iblockstateup = worldIn.getBlockState(pos.up());
+        Block blockup = iblockstateup.getBlock();
+        
+        if(!(blockup instanceof BlockCustomBed && block instanceof BlockCustomBed))
+        {
+        	return false;
+        }
+        
+        Boolean isSameDirection = iblockstate.getValue(FACING) == iblockstateup.getValue(FACING);
+        Boolean isSamePart = iblockstate.getValue(PART) == iblockstateup.getValue(PART);
+
+        return isSamePart.booleanValue() == isSameDirection.booleanValue();
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the actual Block state of this Block at the given position. This applies properties not visible in the
+     * metadata, such as fence connections.
+     */
+    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+    {
+        return state.withProperty(BUNK, Boolean.valueOf(this.canConnectTo(worldIn, pos)));
+    }
+
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {BUNK, FACING, PART, OCCUPIED});
     }
 }
