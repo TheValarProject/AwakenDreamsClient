1f9798ce86826ccb9f4b1bc79d056dfdd6afe90d
MartyParty21 <martin.rokyta@seznam.cz>
2018-02-24 17:38:21 +0100

    Edit Noldor Chestplate recipe

--- mcp/src/minecraft/com/elementfx/tvp/ad/item/crafting/ElvenCraftingManager.java
+++ mcp/src/minecraft/com/elementfx/tvp/ad/item/crafting/ElvenCraftingManager.java
@@ -40,7 +40,7 @@ public class ElvenCraftingManager extends CustomCraftingManager
     	this.addRecipe(new ItemStack(Items.ELVEN_LONG_SWORD, 1), new Object[] {"I", "I", "S", 'S', Items.STICK, 'I', Items.IRON_INGOT});
     	this.addRecipe(new ItemStack(Items.RIVENDELL_SWORD, 1), new Object[] {"I", "I", "S", 'S', Items.STICK, 'I', Items.DIAMOND});
     	this.addRecipe(new ItemStack(Items.ELF_DAGGER, 1), new Object[] {"I", "S", 'S', Items.STICK, 'I', Items.IRON_INGOT});
-    	this.addRecipe(new ItemStack(Items.NOLDOR_CHESTPLATE, 1), new Object[] {"I I", "IGI", "IGI", 'G', Items.GOLD_INGOT, 'I', Items.DIAMOND});
+    	this.addRecipe(new ItemStack(Items.NOLDOR_CHESTPLATE, 1), new Object[] {"I I", "IMI", "IGI", 'G', Items.GOLD_INGOT, 'I', Items.DIAMOND, 'M', Items.MITHRIL_INGOT});
     	Collections.sort(this.getRecipeList(), new Comparator<IRecipe>()
         {
             public int compare(IRecipe p_compare_1_, IRecipe p_compare_2_)
