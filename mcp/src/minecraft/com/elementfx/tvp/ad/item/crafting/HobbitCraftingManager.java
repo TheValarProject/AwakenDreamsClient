53e295e9f0426a02ed3d1ff3c6fe180f721a09fd
MartyParty21 <martin.rokyta@seznam.cz>
2018-02-24 18:10:27 +0100

    Add Hobbit weapon recipes

--- mcp/src/minecraft/com/elementfx/tvp/ad/item/crafting/HobbitCraftingManager.java
+++ mcp/src/minecraft/com/elementfx/tvp/ad/item/crafting/HobbitCraftingManager.java
@@ -11,6 +11,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
@@ -37,6 +38,11 @@ public class HobbitCraftingManager extends CustomCraftingManager
 
     protected HobbitCraftingManager()
     { 	
+    	this.addRecipe(new ItemStack(Items.HOBBIT_SWORD, 1), new Object[] {"I", "I", "S", 'S', Items.STICK, 'I', Items.IRON_INGOT});
+    	this.addRecipe(new ItemStack(Items.HOBBIT_AXE, 1), new Object[] {"II", "SI", "S ", 'S', Items.STICK, 'I', Items.IRON_INGOT});
+    	this.addRecipe(new ItemStack(Items.HOBBIT_DAGGER, 1), new Object[] {"W", "S", 'S', Items.STICK, 'W', Blocks.PLANKS});
+    	this.addRecipe(new ItemStack(Items.HOBBIT_HAMMER, 1), new Object[] {"I", "S", 'S', Items.STICK, 'I', Items.IRON_INGOT});
+    	this.addRecipe(new ItemStack(Items.SHIRRIFF_CLUB, 1), new Object[] {"X", "S", 'S', Items.STICK, 'X', Blocks.PLANKS});
     	Collections.sort(this.getRecipeList(), new Comparator<IRecipe>()
         {
             public int compare(IRecipe p_compare_1_, IRecipe p_compare_2_)
